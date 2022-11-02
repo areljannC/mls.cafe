be5cb8f4bffa8d56dc6668b028e1ee62cafef8e0
AJ Clemente <areljann.c@gmail.com>
2022-11-02 16:59:14 -0400

    Set `textAlign` to `center`

--- client/src/containers/home-container/home-container.tsx
+++ client/src/containers/home-container/home-container.tsx
@@ -5,9 +5,15 @@ import { VStack, Heading, Text } from '@chakra-ui/react';
 // Component
 const HomeContainer = () => (
   <VStack as='main' width='100%' height='100%' justifyContent='center' alignItems='center'>
-    <Heading as='h1' size='2xl'>mls.cafe</Heading>
-    <Text as='p'>🏠 A place to discuss and review real estate listings in Canada.</Text>
-    <Text as='p'>Coming soon early 2023.</Text>
+    <Heading as='h1' size='2xl' textAlign='center'>
+      mls.cafe
+    </Heading>
+    <Text as='p' textAlign='center'>
+      🏠 A place to discuss and review real estate listings in Canada.
+    </Text>
+    <Text as='p' textAlign='center'>
+      Coming soon early 2023.
+    </Text>
   </VStack>
 );
 
